afed6d9838bfe8195573d88db95b149cbb72aadd
Marcin Kaczmarek <mkacz91@gmail.com>
2016-07-22 23:12:30 +0200

    Set up Sheet API

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
+
 $(function() {
-  var mapKey = getParam('key')
-  $.getScript("https://maps.googleapis.com/maps/api/js?key=" + mapKey + "&callback=initMap")
+  gapiKey = getParam('key');
+  if (gapiKey === null) {
+    console.error("Google API key not provided");
+    return;
+  }
+  sheetId = getParam('sheet');
+  if (sheetId === null) {
+    console.error("Sheet ID not provided");
+    return;
+  }
+  loadMapsApi();
 })
+
+var gapiKey;
+var sheetId;
+
+function allApisLoaded() {
+  console.log("Apis ready");
+
+  gapi.client.sheets.spreadsheets.get({
+    spreadsheetId: sheetId,
+    key: gapiKey
+  }).then(function(response) {
+      console.log(response);
+  });
+
+  initMap();
+}
 
 function getParam(name) {
   var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+  if (results === null)
+    return null;
 	return results[1] || 0;
+}
+
+function apiLoaded() {
+  if (!window.pendingApiCount)
+    window.pendingApiCount = 2;
+  if (window.pendingApiCount <= 0) {
+    console.error("pendingApiCount decremented below 0");
+    return;
+  }
+  if (--window.pendingApiCount == 0)
+    allApisLoaded();
+}
+
+function loadMapsApi() {
+  $.getScript('https://maps.googleapis.com/maps/api/js?key=' + gapiKey + '&callback=apiLoaded');
+}
+
+function loadSheetsApi() {
+  gapi.client.load('https://sheets.googleapis.com/$discovery/rest?version=v4').then(apiLoaded);
 }
 
 var map
